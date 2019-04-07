100289f031bafd75add8c0b36006b798f2268380
Roman Verteletsky <verteletsky@bitrix24.ru>
2019-04-07 20:35:51 +0400

    Added response for string

--- common.go
+++ common.go
@@ -104,6 +104,11 @@ type IntResponse struct {
 	Error    *ErrorDto `json:"error"`
 }
 
+type StringResponse struct {
+	Response string    `json:"response"`
+	Error    *ErrorDto `json:"error"`
+}
+
 func (r *IntResponse) ErrorDto() *ErrorDto {
 	return r.Error
 }
